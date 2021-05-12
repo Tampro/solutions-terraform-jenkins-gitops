ef7b0fc27231a05e0237d029f6d4cf70375ccb32
im.pavithranagaraj <42617512+pavithranagaraj26@users.noreply.github.com>
2021-05-12 12:17:49 +0530

    Update main.tf

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -183,12 +183,12 @@ resource "google_project_iam_member" "jenkins-project" {
 }
 
 data "local_file" "helm_chart_values" {
-  filename = "${path.module}/values.yaml"
+  filename = "${path.module}/values1.yaml"
 }
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-  repository = "https://charts.helm.sh/stable"
-  chart      = "jenkins"
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins/jenkins"
  # version    = "1.9.18"
   timeout    = 1200
   values = [data.local_file.helm_chart_values.content]
